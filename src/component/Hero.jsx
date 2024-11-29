4a71fb1b5b497623e63f46c1e91f2240857d9684
Aditya rai <raijiaditya9868@gmail.com>
2024-11-29 16:47:43 +0530

    m

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "/src/assets/kevinRushProfile.png"
+import profilePic from "../assets/profile.png"
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
