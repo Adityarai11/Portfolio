1e0a9a38adeecec649aab758f5c6d3bd04512d46
Aditya rai <raijiaditya9868@gmail.com>
2025-04-29 17:37:44 +0530

    changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,18 +1,19 @@
 import logo from "../assets/logo.webp";
-import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 const Navbar = () => {
   // Define the social media URLs
   const socialLinks = {
-    linkedin: "https://www.linkedin.com/in/aditya-rai2003", // Replace with your LinkedIn profile link
-    github: "https://github.com/Adityarai11",             // Replace with your GitHub profile link
-    twitter: "https://x.com/Adityarai1111?t=TINKufCtaRqtzqeVdIJr7w&s=09", // Replace with your Twitter profile link
-    instagram: "https://www.instagram.com/adirai_adi/profilecard/?igsh=MWRub2UzdTYyNWJraw==", // Replace with your Instagram profile link
+    linkedin: "https://www.linkedin.com/in/aditya-rai2003",
+    github: "https://github.com/Adityarai11",
+    twitter: "https://x.com/Adityarai1111?t=TINKufCtaRqtzqeVdIJr7w&s=09",
+    instagram: "https://www.instagram.com/adirai_adi/profilecard/?igsh=MWRub2UzdTYyNWJraw==",
+    whatsapp: "https://wa.me/919354180635", // WhatsApp link in international format
   };
 
   // Function for handling logo click to refresh the page
   const handleLogoClick = () => {
-    window.location.reload(); // Reloads the current page
+    window.location.reload();
   };
 
   return (
@@ -36,9 +37,4 @@ const Navbar = () => {
         <a href={socialLinks.twitter} target="_blank" rel="noopener noreferrer">
           <FaTwitter />
         </a>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
+        <a href={socialLinks.whatsapp} target="
